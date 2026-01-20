d3bbdaee192f088126842ee15e8283ee1b3174fb
Moses Henen <155010796+dev-henen@users.noreply.github.com>
2026-01-20 18:42:59 +0100

    fix(api): read cookies in get user endpoint

--- src/routes/api/users/get_user/+server.ts
+++ src/routes/api/users/get_user/+server.ts
@@ -1,18 +1,20 @@
-import { basicAccountAuth, getAccountEndpoint } from "$lib/server/utilities.js";
-import { json } from "@sveltejs/kit";
+import { basicAccountAuth, getAccountEndpoint } from '$lib/server/utilities.js';
+import { json } from '@sveltejs/kit';
 
 /** @type {import('./$types').RequestHandler} */
-export async function GET({ url, fetch }) {
-    const parsedUrl = new URL(url);
-    const queryParams = Object.fromEntries(parsedUrl.searchParams);
-    const user_token = queryParams.user_token;
-    const payload = {
-        user_token: user_token
-    };
-    const endpoint = getAccountEndpoint("/users/get_user");
-    const res = await fetch(endpoint, basicAccountAuth('GET', payload));
+export async function GET({ fetch, cookies }) {
+	const user_token = cookies.get('user_token');
+	if (!user_token) {
+		return json({ error: 'User token not found' });
+	}
 
-    const result = await res.text();
+	const payload = {
+		user_token: user_token
+	};
+	const endpoint = getAccountEndpoint('/users/get_user');
+	const res = await fetch(endpoint, basicAccountAuth('GET', payload));
 
-    return json(result);
+	const result = await res.text();
+
+	return json(result);
 }
