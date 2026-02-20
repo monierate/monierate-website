ff95cf9dd2dabae5d6ed6a7eaec0a5fc03654b83
Moses Henen <155010796+dev-henen@users.noreply.github.com>
2026-02-20 11:12:34 +0100

    Update plan name styling for better visibility and consistency

--- src/lib/components/Pricing.svelte
+++ src/lib/components/Pricing.svelte
@@ -26,7 +26,7 @@
 									? 'text-blue-500 bg-blue-800/10'
 									: i === 2
 										? 'text-green-500 bg-green-800/10'
-										: 'text-gray-800 dark:text-gray-200 bg-gray-800'} rounded-lg p-1 px-4"
+										: 'text-gray-800 dark:text-gray-200 bg-gray-200 dark:bg-gray-800'} rounded-lg p-1 px-4"
 						>
 							{plan.name}
 						</span>
