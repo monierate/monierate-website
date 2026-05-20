ab74a7bfc3c78baba45860c81ccf92cb27b5cae1
Moses Henen <155010796+dev-henen@users.noreply.github.com>
2026-05-20 03:38:49 +0100

    Update top banner to replace Zobo with Vban and adjust styles

--- src/lib/components/banners/TopBanner.svelte
+++ src/lib/components/banners/TopBanner.svelte
@@ -6,19 +6,12 @@
 
 	const sponsored_partners = [
 		{
-			image: 'https://www.zobo.money/?ref=monierate&utm_source=monierate&utm_medium=referral',
-			text: 'Send money to Africa. Zero fees, great rates.',
-			link: 'https://www.zobo.money/?ref=monierate&utm_source=monierate&utm_medium=referral',
-			brand: 'Zobo',
-			cta: 'Send Money'
+			image: 'https://vban.com/?ref=monierate&utm_source=monierate&utm_medium=referral',
+			text: 'Get Paid Globally with Dollar',
+			link: 'https://vban.com/?ref=monierate&utm_source=monierate&utm_medium=referral',
+			brand: 'Vban',
+			cta: 'Download'
 		}
-		// {
-		//     image: 'https://bit.ly/3svJGhQ',
-		//     text: 'Send money to over 80 countries worldwide.',
-		//     link: 'https://tinyurl.com/cambridge-currences-top-banner',
-		//     brand: 'Cambridge Currencies',
-		//     cta: 'Send Money'
-		// }
 	];
 	const selected_partner_top = selectTopPartnerBanner(sponsored_partners);
 </script>
@@ -28,7 +21,7 @@
 <div
 	id="top-banner"
 	tabindex="-1"
-	class="flex fixed top-0 mb-8 w-full z-50 gap-x-6 overflow-hidden bg-[#ed0578] px-6 py-5 sm:px-3.5 sm:before:flex-1 whitespace-nowrap"
+	class="flex fixed top-0 mb-8 w-full z-50 gap-x-6 overflow-hidden bg-[#026DFC] px-6 py-5 sm:px-3.5 sm:before:flex-1 whitespace-nowrap"
 >
 	<div class="flex flex-wrap items-center gap-x-4 gap-y-2">
 		<span class="text-sm leading-6 text-gray-100">
