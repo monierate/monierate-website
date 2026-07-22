0daf37829f84c16257a42c26d71b7d61a3e87f91
Moses Henen <155010796+dev-henen@users.noreply.github.com>
2026-07-22 12:06:38 +0100

    fix: update LoadingIndicator styles for improved visibility

--- src/lib/components/LoadingIndicator.svelte
+++ src/lib/components/LoadingIndicator.svelte
@@ -7,8 +7,8 @@
 </script>
 
 {#if isNavigating}
-	<div class="fixed top-0 left-0 w-full h-1 z-[1000]">
-		<div class="h-full bg-primary origin-left animate-progress-bar"> </div>
+	<div class="fixed top-0 left-0 w-full h-1.5 z-[2147483647]">
+		<div class="h-full bg-[var(--page-bg)] origin-left animate-progress-bar"></div>
 	</div>
 {/if}
 
